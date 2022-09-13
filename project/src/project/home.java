ab36f6a6dfb99c9d5088aecab3187780dae91c8f
varuncloudroots <112550324+varuncloudroots@users.noreply.github.com>
2022-09-13 12:28:02 +0530

    Commit Push

--- project/src/project/home.java
+++ project/src/project/home.java
@@ -1,11 +1,14 @@
 package project;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 public class home {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    System.out.println("hello world");
-    System.out.println("my name is varun");
+		System.setProperty("WebDriver.Chrome.driver","C:\\Selenium\\chromedriver_win32\\chromedriver.exe");
+    WebDriver ER = new ChromeDriver();
 	}
-
 }
